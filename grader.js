c47cfbb1f961f280b57fdc9599c282c5fa89ca52
Albert <albert.li@fox.com>
2015-08-26 19:49:35 -0700

    reset

--- grader.js
+++ grader.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
+
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
 and basic DOM parsing.
-
+one
+two
 References:
 
  + cheerio
